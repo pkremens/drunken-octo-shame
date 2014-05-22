2336f39b4ae39afab7f23710b78d8b5374b5f071
Petr Kremensky <pkremens@redhat.com>
2014-05-22 12:12:49 +0200

    Added jvm testing utility

---
+++ src/main/java/your/simple/java/gid/App.java
@@ -0,0 +1,34 @@
+package your.simple.java.gid;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("");
+        int mb = 1024 * 1024;
+
+        //Getting the runtime reference from system
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println("##### Heap utilization statistics [MB] #####");
+
+        //Print used memory
+        System.out.println("Used Memory:"
+                + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+
+        //Print free memory
+        System.out.println("Free Memory:"
+                + runtime.freeMemory() / mb);
+
+        //Print total available memory
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+
+        //Print Maximum available memory
+        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+
+        //Get the JDK architecture
+        System.out.println("JVM Bit size: " + System.getProperty("sun.arch.data.model"));
+    }
+}
