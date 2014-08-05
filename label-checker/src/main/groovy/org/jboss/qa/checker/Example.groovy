167ff39ba73a9707ca0ce26741023ea58f95d47b
Petr Kremensky <pkremens@redhat.com>
2014-08-05 09:58:06 +0200

    Added label-checker groovy project

---
+++ label-checker/src/main/groovy/org/jboss/qa/checker/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package org.jboss.qa.checker
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
