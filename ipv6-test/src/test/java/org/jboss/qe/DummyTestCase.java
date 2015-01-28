fec926953601037ce0dd08e1a32f3b8b00a90a51
Petr Kremensky <pkremens@redhat.com>
2015-01-28 12:42:02 +0100

    Adding simple junit projet for IPv6 hosts testing

---
+++ ipv6-test/src/test/java/org/jboss/qe/DummyTestCase.java
@@ -0,0 +1,13 @@
+package org.jboss.qe;
+
+import org.junit.Test;
+
+/**
+ * @author Petr Kremensky pkremens@redhat.com on 1/28/15.
+ */
+public class DummyTestCase {
+    @Test
+    public void dummyTest() {
+        System.out.println("Executing the dummy test.");
+    }
+}
