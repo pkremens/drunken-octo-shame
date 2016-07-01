a21b8430006776f92f7a7c9f3b9e93a83e83586b
Petr Kremensky <pkremens@redhat.com>
2016-07-01 17:53:07 +0200

    Expected exception test case

--- junit-examples/src/test/java/cz/hradek/kremilek/junit/examples/rules/ExpectedExceptionTestCase.java
+++ junit-examples/src/test/java/cz/hradek/kremilek/junit/examples/rules/ExpectedExceptionTestCase.java
@@ -1,5 +1,12 @@
 package cz.hradek.kremilek.junit.examples.rules;
 
+import cz.hradek.kremilek.logging.LoggerFactory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.logging.Logger;
+
 /**
  * http://meri-stuff.blogspot.cz/2014/08/junit-rules.html#UsingRulesExampleRuleTemporaryFolder
  * <p>
@@ -15,4 +22,22 @@ package cz.hradek.kremilek.junit.examples.rules;
  * @author Petr Kremensky pkremens@redhat.com
  */
 public class ExpectedExceptionTestCase {
+    private static final Logger log = LoggerFactory.getLogger(ExpectedExceptionTestCase.class.getSimpleName());
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void noException() {
+        log.info("No Exception is expected in this test.");
+    }
+
+    @Test
+    public void NPETest() {
+        String obj = null;
+
+        thrown.expect(NullPointerException.class);
+        log.info("Throw NPE and pass the test, no more code is executed once the exception is thrown!");
+        obj.split("/n");
+        log.warning("Once the exception is thrown, the rest of the code is not executed!!!!");
+    }
 }
