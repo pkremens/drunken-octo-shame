c978a0a1a9d39e5b14cdb561be8ced26abdd1d2a
Petr Kremensky <pkremens@redhat.com>
2019-04-12 08:11:28 +0200

    Add JUnitSoftAssertions example

---
+++ junit-examples/src/test/java/cz/hradek/kremilek/junit/examples/soft/SoftAssertionsTestCase.java
@@ -0,0 +1,28 @@
+package cz.hradek.kremilek.junit.examples.soft;
+
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Petr Kremensky pkremens@redhat.com
+ */
+public class SoftAssertionsTestCase {
+    @Rule
+    public JUnitSoftAssertions softAssertions = new JUnitSoftAssertions();
+
+    @Test
+    public void verifiesScoreSoftlyUsingRule() {
+        String fails = "Sorry, I've got to fail!";
+
+        softAssertions.assertThat(fails)
+                .as("Have size")
+                .hasSize(8);
+        softAssertions.assertThat(fails)
+                .as("Start with")
+                .startsWith("Congrats,");
+        softAssertions.assertThat(fails)
+                .as("Doesn't contain \"fail\" string")
+                .doesNotContain("fail");
+    }
+}
